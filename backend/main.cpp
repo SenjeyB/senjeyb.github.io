c7a82624bd656b7476bb31b61a0afc8efc634ec5
SenjeyB <SenjeyB@users.noreply.github.com>
2024-09-03 10:43:43 +0000

    deploy: 9161ea23273f233d352e16b8067d27385e84a896

--- backend/main.cpp
+++ backend/main.cpp
@@ -18,41 +18,48 @@ void fetchDataAndProcess() {
     curl_global_init(CURL_GLOBAL_ALL);
     curl = curl_easy_init();
     if (curl) {
-        curl_easy_setopt(curl, CURLOPT_URL, "https://thronebutt.com/api/v0/get/daily");
-        curl_easy_setopt(curl, CURLOPT_POST, 1L);
-        curl_easy_setopt(curl, CURLOPT_WRITEFUNCTION, WriteCallback);
-        curl_easy_setopt(curl, CURLOPT_WRITEDATA, &readBuffer);
-
-        struct curl_slist* headers = NULL;
-        headers = curl_slist_append(headers, "Content-Type: application/x-www-form-urlencoded");
-        curl_easy_setopt(curl, CURLOPT_HTTPHEADER, headers);
-
-        res = curl_easy_perform(curl);
-        if (res != CURLE_OK) {
-            std::cerr << "curl_easy_perform() failed: " << curl_easy_strerror(res) << std::endl;
-        } else {
-            try {
-                nlohmann::json jsonData = nlohmann::json::parse(readBuffer);
-
-                const auto& entries = jsonData["entries"];
-                Database db("players.db");
-
-                int totalPlayers = entries.size();
-                for (const auto& entry : entries) {
-                    int rank = entry["rank"].get<int>();
-                    std::string name = entry["name"].get<std::string>();
-                    int score = entry["score"].get<int>();
-                    int64_t steamid = entry["steamid"].get<int64_t>();
-
-                    int adjustedScore = ceil((double)((totalPlayers + 1 - rank) * 1000) / (double)totalPlayers);
-                    db.updatePlayer(steamid, name, adjustedScore, score);
-                    std::cout << "id update: " << steamid << std::endl;
+        for(int p = 1;; p++)
+        {
+            char postData[50];
+            snprintf(postData, sizeof(postData), "p=%ld", p);
+            
+            curl_easy_setopt(curl, CURLOPT_URL, "https://thronebutt.com/api/v0/get/daily");
+            curl_easy_setopt(curl, CURLOPT_POST, 1L);
+            curl_easy_setopt(curl, CURLOPT_POSTFIELDS, postData);
+            curl_easy_setopt(curl, CURLOPT_WRITEFUNCTION, WriteCallback);
+            curl_easy_setopt(curl, CURLOPT_WRITEDATA, &readBuffer);
+    
+            struct curl_slist* headers = NULL;
+            headers = curl_slist_append(headers, "Content-Type: application/x-www-form-urlencoded");
+            curl_easy_setopt(curl, CURLOPT_HTTPHEADER, headers);
+    
+            res = curl_easy_perform(curl);
+            if (res != CURLE_OK) {
+                break;
+            } else {
+                try {
+                    nlohmann::json jsonData = nlohmann::json::parse(readBuffer);
+    
+                    const auto& entries = jsonData["entries"];
+                    Database db("players.db");
+    
+                    int totalPlayers = entries.size();
+                    for (const auto& entry : entries) {
+                        int rank = entry["rank"].get<int>();
+                        std::string name = entry["name"].get<std::string>();
+                        int score = entry["score"].get<int>();
+                        int64_t steamid = entry["steamid"].get<int64_t>();
+    
+                        int adjustedScore = ceil((double)((totalPlayers + 1 - rank) * 1000) / (double)totalPlayers);
+                        db.updatePlayer(steamid, name, adjustedScore, score);
+                        std::cout << "id update: " << steamid << std::endl;
+                    }
+                } catch (nlohmann::json::parse_error& e) {
+                    std::cerr << "Failed to parse JSON: " << e.what() << std::endl;
                 }
-            } catch (nlohmann::json::parse_error& e) {
-                std::cerr << "Failed to parse JSON: " << e.what() << std::endl;
             }
+            curl_easy_cleanup(curl);
         }
-        curl_easy_cleanup(curl);
     }
     curl_global_cleanup();
 }
