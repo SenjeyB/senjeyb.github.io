a76a778437c16b2fb9797e81f0efb7d80b900a15
Senjey <65812844+SenjeyB@users.noreply.github.com>
2024-09-03 22:33:05 +0300

    add css

--- generate_html.py
+++ generate_html.py
@@ -11,7 +11,7 @@ HTML_TEMPLATE = '''<!DOCTYPE html>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Top Players - Page {{ page }}</title>
-    <link rel="stylesheet" href="styles.css">
+    <link rel="stylesheet" href="css/styles.css">
 </head>
 <body>
     <h1>Top Players - Page {{ page }}</h1>
