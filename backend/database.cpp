1dcc1921a54644fed9e84682a727e9c63b325335
SenjeyB <SenjeyB@users.noreply.github.com>
2024-09-03 15:31:32 +0000

    deploy: 88a30f1e5ab4194bea4b0f6fddb7495442a7f3e0

--- backend/database.cpp
+++ backend/database.cpp
@@ -5,7 +5,10 @@
 class Database {
 public:
     Database(const std::string& dbPath) {
-        sqlite3_open(dbPath.c_str(), &db);
+        if (sqlite3_open(dbPath.c_str(), &db) != SQLITE_OK) {
+            std::cerr << "Error opening database: " << sqlite3_errmsg(db) << std::endl;
+            db = nullptr;  // Убедитесь, что db указывает на nullptr при ошибке
+        }
     }
 
     ~Database() {
@@ -24,7 +27,12 @@ public:
 
         if (exists) {
             std::string updateQuery = "UPDATE players SET name = ?, score = score + ?, killcount = killcount + ? WHERE id = ?;";
-            sqlite3_prepare_v2(db, updateQuery.c_str(), -1, &stmt, nullptr);
+            
+            int rc = sqlite3_prepare_v2(db, updateQuery.c_str(), -1, &stmt, nullptr);
+if (rc != SQLITE_OK) {
+    std::cerr << "Error preparing statement: " << sqlite3_errmsg(db) << std::endl;
+    return; // или обработать ошибку иным способом
+}
             sqlite3_bind_text(stmt, 1, name.c_str(), -1, SQLITE_STATIC);
             sqlite3_bind_int(stmt, 2, newScore);
             sqlite3_bind_int(stmt, 3, score);
@@ -37,8 +45,9 @@ public:
             sqlite3_bind_int(stmt, 3, newScore);
             sqlite3_bind_int(stmt, 4, score);
         }
-
-        sqlite3_step(stmt);
+        if (sqlite3_step(stmt) != SQLITE_DONE) {  // Этот вызов выполняет запрос
+            std::cerr << "Error updating player: " << sqlite3_errmsg(db) << std::endl;
+        }
         sqlite3_finalize(stmt);
     }
 
